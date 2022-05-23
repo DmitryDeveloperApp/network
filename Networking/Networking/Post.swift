b52f2e8465c72863f482503af2b3cad23ff237d6
Dmitry <DmitryDeveloperApp@gmail.com>
2022-05-23 14:34:10 +0300

    Tests GET requst

--- Networking/Networking/Post.swift
+++ Networking/Networking/Post.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+class Post: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+    
+}
--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var networkManager = NetworkManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
-
+    @IBAction func dowloadPostsDidTap() {
+        
+        networkManager.getAllPosts { [weak self] (posts) in
+            DispatchQueue.main.async {
+                self?.titleLabel.text = "Post has been downloader"
+            }
+        }
+        
+    }
 }
 
